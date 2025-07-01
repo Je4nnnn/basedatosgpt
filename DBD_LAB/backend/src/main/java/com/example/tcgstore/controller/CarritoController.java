a5b9c8cc901fc036e6c5391e6bd2540cfd19ece0
Jean Lucas Dupraz Lopez <154584400+Je4nnnn@users.noreply.github.com>
2025-07-01 06:06:04 -0400

    Add Carrito entity and fix service/controller

--- DBD_LAB/backend/src/main/java/com/example/tcgstore/controller/CarritoController.java
+++ DBD_LAB/backend/src/main/java/com/example/tcgstore/controller/CarritoController.java
@@ -1,5 +1,6 @@
 package com.example.tcgstore.controller;
 
+import com.example.tcgstore.model.Carrito;
 import com.example.tcgstore.service.CarritoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,17 +15,17 @@ public class CarritoController {
     private CarritoService carritoService;
 
     @GetMapping
-    public List<BoletaService.Carrito> listarCarritos() {
+    public List<Carrito> listarCarritos() {
         return carritoService.obtenerTodos();
     }
 
     @GetMapping("/{id}")
-    public BoletaService.Carrito obtenerPorId(@PathVariable Integer id) {
+    public Carrito obtenerPorId(@PathVariable Integer id) {
         return carritoService.obtenerPorId(id);
     }
 
     @PostMapping
-    public BoletaService.Carrito crear(@RequestBody BoletaService.Carrito carrito) {
+    public Carrito crear(@RequestBody Carrito carrito) {
         return carritoService.guardar(carrito);
     }
 
